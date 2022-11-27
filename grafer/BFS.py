85864c913b777428874d2c1530d5bbc641a96c09
jonathan <jonathan.cederlund@gmail.com>
2022-11-27 14:54:35 +0100

    added Dijikstras and BFS

---
+++ grafer/BFS.py
@@ -0,0 +1,42 @@
+#Graph = [[1,2,3],[0],[0,3],[0,4],[3,7],[7],[7],[4,5,6]]
+#0, 1,1,1,2,4,4,3
+
+def bfs(Graph, S):
+    q = [S]
+    dist = {S: 0}
+    while q:
+        q2 = []
+        for u in q:
+            for v in Graph[u]:
+                if v not in dist:
+                    dist[v] = dist[u] + 1
+                    q2.append(v)
+        q = q2
+    return dist
+
+
+Graph = [[1, 2, 3], [0], [0, 3], [0, 4], [3, 7], [7], [7], [4, 5, 6]]
+dists = bfs(Graph, 0)
+
+print(dists)
+
+
+def bfs2(grid, r, c):
+    R = len(grid)
+    C = len(grid[0])
+    q = [(r, c)]
+    dist = {(r, c): 0}
+    while q:
+        q2 = []
+        for r, c in q:
+            for nr, nc in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                if 0 <= nr < R and 0 <= nc < C:
+                    tup = nr, nc
+                    if grid[nr, nc] != '#' and tup not in dist:
+                        dist[tup] = dist[r, c] + 1
+                        q2.append(tup)
+        q = q2
+    return dist
+
+
+grid = {}
---
+++ grafer/Dijikstras.py
@@ -0,0 +1,38 @@
+from heapq import heappush, heappop
+# s : startNod
+#g: [[(to_node,wight), ... ] , ...]
+INF = 10**9
+
+
+def dijkstra(s, T, G):
+    dists = [INF]*len(G)
+    parent = [None]*len(G)
+    heap = []
+    heappush(heap, (0, s))
+    dists[s] = 0
+    while heap:
+        d, u = heappop(heap)
+        if u == T:
+            return d  # när man vill gå från s till t
+        if d > dists[u]:
+            continue
+        for v, c in G[u]:
+            alt = d + c
+            if alt < dists[v]:
+                dists[v] = alt
+                parent[v] = u
+                heappush(heap, (alt, v))
+
+    if dists[T] == INF:
+        return None
+    path = []
+    curr = T
+    while curr != None:
+        path.append(curr)
+        curr = parent(curr)
+
+    # return path[::-1]
+    return dists
+
+
+G = [[(1, 3), (2, 1)], []]
