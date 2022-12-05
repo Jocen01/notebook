f5d8fcc6f456bc57e09cc92f113d506bd278ab8f
jonathan <jonathan.cederlund@gmail.com>
2022-12-05 16:20:05 +0100

    added alternative topsort that catches loops

--- grafer/DAG.py
+++ grafer/DAG.py
@@ -16,10 +16,34 @@ def createDAG(graph,nodes):
                 prosesed.add(p)
     return DAG
 
+def topSort(graph,nodes):
+    from collections import defaultdict
+    DAG = []
+    state = defaultdict(lambda: 0)
+    prosesing = []  
+    for i in nodes:
+        if state[i] == 2: continue
+        prosesing.append(i)
+        state[i] = 1
+        while prosesing:
+            p = prosesing[-1]
+            for to in graph[p]:
+                if state[to] == 1:
+                    return []
+                if state[to] != 2:
+                    prosesing.append(to)
+                    state[to] = 1
+                    break
+            if p == prosesing[-1]:
+                p = prosesing.pop()
+                DAG.append(p)
+                state[p] = 2
+    return DAG
+
 def getLongestPathDAG(DAG, graph):
-    path = {DAG[0]: (1,-1)}
-    f = (1, DAG[0])
-    FR = DAG[0]
+    path = {DAG[0]: (1,-1)} #dict with the node -> next node 
+    f = (1, DAG[0]) # f[0] is the length of the longest path in dag
+    FR = DAG[0] # FR is the point at witch the longest path begins
     for i in DAG:
         if i in path: continue
         longest = (1,-1)
