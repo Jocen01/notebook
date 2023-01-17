b7eba441b04300a0e97a7f019202e98b38a21d63
jonathan <jonathan.cederlund@gmail.com>
2023-01-17 15:16:15 +0100

    added miller-rabin primality test

--- geometri/find_prime.py
+++ geometri/find_prime.py
@@ -1,8 +1,31 @@
-n = 2
-i = 1
-while n * n <= k:
-    if k % n == 0:
-        k = k/n
-        i += 1
-    else:
-        n += 2
+N = int(input())
+A = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37] #räcker för upp till 10**18 annars är det bara lägga till några fler primtal
+d = N - 1
+s = 0
+if N < 100: #kolla de låga primtalen
+    for i in range(2,N):
+        if N%i == 0:
+            print("NO")
+            exit()
+    print("YES")
+    exit()
+if N % 2 == 0:
+    print("NO")
+    exit()
+while d % 2 == 0:
+    s+= 1
+    d = d//2
+# Miller–Rabin primality test
+for a in A:
+    X = pow(a, d, N)
+    Y = 1
+    for _ in range(s):
+        Y = pow(X, 2, N)
+        if Y == 1 and X != 1 and X != N - 1:
+            print("NO")
+            exit()
+        X = Y
+    if Y != 1:
+        print("NO")
+        exit()
+print("YES")
